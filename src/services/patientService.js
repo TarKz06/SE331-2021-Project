209bde59dd3dd212600778514d933ece306ece4a
teambenq <teetanic27@gmail.com>
2021-08-08 21:48:26 +0700

    add service

---
+++ src/services/patientService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3004',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+  },
+  //Add new call
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  }
+}
