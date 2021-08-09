e6b00562e51460a1eda5e1db28ed0f3d4d477890
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-08-09 21:07:57 +0700

    commentFrom

---
+++ src/components/commentForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="review-form" @submit.prevent="onSubmit">
+        
+        <label for="comment">Doctor's comment: </label>
+        <textarea id="comment" v-model="comment"></textarea>
+        
+        <input class="button" type="submit" value="Submit">
+    </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            comment:'',
+        }
+    },
+    methods: {
+        onSubmit(){
+            if (this.comment === '') {
+                alert('Comment is incomplete. Please fill out every field.')
+                return
+            }
+            let productReview = {
+                comment: this.comment
+            }
+            this.$emit('submitted',productReview)
+            this.comment = ''
+        }
+    }
+}
+</script>
---
+++ src/components/commentList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="review-container">
+        <h3>Comments</h3>
+        <ul>
+            <li v-for="(comment,index) in comments" :key="index">
+                "{{comment.comment}}"
+                <br/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        comments:{
+            type:Array,
+            required: true
+        }
+    },
+}
+</script>
