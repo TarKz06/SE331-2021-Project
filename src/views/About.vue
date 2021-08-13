f4397f3e5c084dd8ced40bb4feeb96c40a61e29d
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-08-13 20:02:01 +0700

    Design by quasar
    add image name and e-mail

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,94 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3 class="h3"><b>About us</b></h3>
+    <h4>Keemo Number One</h4>
   </div>
-    <q-avatar size="100px" font-size="52px" color="teal" text-color="white" icon="directions" />
+  <div class="q-pa-md row items-start q-gutter-md">
+    <div class="row justify-center q-gutter-sm">
+      <q-card class="my-card">
+        <img src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t1.6435-9/80650660_1513612772127181_2329682882320138240_n.jpg?_nc_cat=106&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=x1New6yJXkYAX_1Zyjr&_nc_ht=scontent.fcnx3-1.fna&oh=1c11f1b67130a8bc462ed2f3d31b0524&oe=613CCBA3">
+
+        <q-card-section>
+          <div class="text-h6">Khematat Supphamittanont</div>
+          <div class="text-subtitle2">Student ID: 622115006</div>
+        </q-card-section>
+
+        <q-separator />
+          <div class="q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--round q-btn--actionable q-focusable q-hoverable">
+              <span class="q-btn__content text-center col items-center q-anchor--skip justify-center row">
+                <i class="notranslate material-icons q-icon" aria-hidden="true" role="img">
+                  mail
+                  <q-menu anchor="top right" self="top left">
+                <q-list style="min-width: 100px">
+                  <q-item clickable v-close-popup>
+                    <q-item-section>khematat_s@elearning.cmu.ac.th</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+                </i>
+              </span>
+          </div>
+      </q-card>
+
+      <q-card class="my-card">
+        <img src="https://cdn.discordapp.com/attachments/640609460494008330/875711863508303882/964275_10200743694507732_1968858864_o.jpg">
+
+        <q-card-section>
+          <div class="text-h6">Teetanic Yaprasert</div>
+          <div class="text-subtitle2">Student ID: 622115018</div>
+        </q-card-section>
+        
+        <q-separator />
+          <div class="q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--round q-btn--actionable q-focusable q-hoverable">
+              <span class="q-btn__content text-center col items-center q-anchor--skip justify-center row">
+                <i class="notranslate material-icons q-icon" aria-hidden="true" role="img">
+                  mail
+                  <q-menu anchor="top right" self="top left">
+                    <q-list style="min-width: 100px">
+                      <q-item clickable v-close-popup>
+                        <q-item-section>teetanic_y@elearning.cmu.ac.th</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </i>
+                
+              </span>
+          </div>
+      </q-card>
+
+      <q-card class="my-card">
+        <img src="https://scontent.fcnx3-1.fna.fbcdn.net/v/t1.6435-9/78640316_957949701240148_6753999883012669440_n.jpg?_nc_cat=101&ccb=1-4&_nc_sid=174925&_nc_ohc=1hsIRQS-4aMAX-ReWXn&_nc_ht=scontent.fcnx3-1.fna&oh=2eb9d615a23e4d7d8efa218e4f434082&oe=613BDB3F">
+
+        <q-card-section>
+          <div class="text-h6">Prin Ritti-traiphop</div>
+          <div class="text-subtitle2">Student ID: 622115023</div>
+        </q-card-section>
+
+        <q-separator />
+        <div class="q-btn q-btn-item non-selectable no-outline q-btn--flat q-btn--round q-btn--actionable q-focusable q-hoverable">
+            <span class="q-btn__content text-center col items-center q-anchor--skip justify-center row">
+              <i class="notranslate material-icons q-icon" aria-hidden="true" role="img">
+                mail
+                <q-menu anchor="top right" self="top left">
+                <q-list style="min-width: 100px">
+                  <q-item clickable v-close-popup>
+                    <q-item-section>prin_r@elearning.cmu.ac.th</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+              </i>
+            </span>
+        </div>
+      </q-card>
+    </div>
+  </div>
 </template>
 
+<style>
+.my-card{
+  width: 31.5%;
+}
+.h3{
+  font-size: 24;
+}
+</style>
